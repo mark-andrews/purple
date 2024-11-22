2445d6a7b3948b9ed847fb14b5c96949dbf01ae4
Mark Andrews <mjandrews.org@gmail.com>
2024-11-22 20:47:35 +0000

    Add renv stuff

--- analysis/compare_behav_and_eeginfo.R
+++ analysis/compare_behav_and_eeginfo.R
@@ -1,9 +1,9 @@
 library(tidyverse)
-library(rutils)
+# library(rutils)
 
 # Get the behav data, trials info from EEG, and combine them
 data_df <- read_results_json("raw-data/pilots/pilot_26July2023/EM_Pilot_07_26_2023_14_53_26_results.json")
-eeg_df <- read_csv('data/pilots/pilot_26July2023/EM_Pilot_07_26_2023_14_53_26_results.csv')
+eeg_df <- read_csv("data/pilots/pilot_26July2023/EM_Pilot_07_26_2023_14_53_26_results.csv")
 data_df2 <- bind_cols(data_df, rename(eeg_df, eeg_block = block))
 
 # do they match?
@@ -12,6 +12,12 @@ data_df2 %>% summarize(all(type == stimulus))
 data_df2 %>% summarize(all(key_pressed == response))
 
 # How about the reaction times?
-data_df2 %>% mutate(rt2 = (stop_tic-1) - start_tic, rt2 = rt2/1024) %>% summarize(mean(abs(rt - rt2)) * 1000)
-data_df2 %>% mutate(rt2 = (stop_tic-1) - start_tic, rt2 = rt2/1024) %>% summarize(max(abs(rt - rt2)) * 1000)
-data_df2 %>% mutate(rt2 = (stop_tic-1) - start_tic, rt2 = rt2/1024) %>% summarize(min(abs(rt - rt2)) * 1000)
+data_df2 %>%
+  mutate(rt2 = (stop_tic - 1) - start_tic, rt2 = rt2 / 1024) %>%
+  summarize(mean(abs(rt - rt2)) * 1000)
+data_df2 %>%
+  mutate(rt2 = (stop_tic - 1) - start_tic, rt2 = rt2 / 1024) %>%
+  summarize(max(abs(rt - rt2)) * 1000)
+data_df2 %>%
+  mutate(rt2 = (stop_tic - 1) - start_tic, rt2 = rt2 / 1024) %>%
+  summarize(min(abs(rt - rt2)) * 1000)
