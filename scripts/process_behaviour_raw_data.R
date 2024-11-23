b0ecec0549434e688eb9922df3426477ec5dbacc
Mark Andrews <mjandrews.org@gmail.com>
2024-11-23 18:16:10 +0000

    Uncomment commented code in one script

--- scripts/process_behaviour_raw_data.R
+++ scripts/process_behaviour_raw_data.R
@@ -1,3 +1,2 @@
-#behaviour_df <- purputils::read_behavioural_results(snakemake@input)
-#readr::write_csv(behaviour_df, file=snakemake@output[[1]])
-
+behaviour_df <- purputils::read_behavioural_results(snakemake@input)
+readr::write_csv(behaviour_df, file = snakemake@output[[1]])
